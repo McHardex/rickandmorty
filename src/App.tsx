fa1d173addd9bff4dd5f853db3fe00a03b80f6f8
mchardex <bukunmiadebisi@gmail.com>
2021-08-19 02:34:35 +0100

    chore: app setup
    - add prettier config
    - add axios configs
    - add interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { Characters, Location, Episode, CharactersInfo } from "Interfaces";
 
-function App() {
+const url = "https://rickandmortyapi.com/api/character";
+
+const App = () => {
+  const [characters, setCharacters] = useState<Characters[]>([]);
+  const [info, setInfo] = useState<CharactersInfo>({
+    pages: 0,
+    count: 0,
+    prev: null,
+    next: null,
+  });
+  const [page, setPage] = useState<number>(1);
+
+  const getLocation = async (locationUrl: string): Promise<Location[]> => {
+    const res = await axios.get(locationUrl);
+    return res.data;
+  };
+
+  const getChapters = async (episodeUrl: string): Promise<Episode[]> => {
+    const res = await axios.get(episodeUrl);
+    return res.data;
+  };
+
+  const getCharactersLocationAndEpisodes =
+    useCallback(async (): Promise<void> => {
+      const res = await axios.get(`${url}/?page=${page}`);
+      const characters = res.data.results;
+
+      const charactersWithLocationAndEpisodeDetails: Characters[] =
+        characters.map(async (character: Characters) => {
+          // get location details
+          const locationDetails = await getLocation(character.location.url);
+
+          // get episode details
+          const episodesPromises = character.episode.map(
+            async (episode) => await getChapters(episode)
+          );
+
+          const episodeDetails = await Promise.all([...episodesPromises]);
+
+          // update charaters
+          return {
+            ...character,
+            location: locationDetails,
+            episode: episodeDetails,
+          };
+        });
+
+      const result = await Promise.all(charactersWithLocationAndEpisodeDetails);
+
+      setInfo(res.data.info);
+      setCharacters(result);
+    }, [page]);
+
+  useEffect(() => {
+    getCharactersLocationAndEpisodes();
+  }, [page, getCharactersLocationAndEpisodes]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {characters.length > 0 && (
+        <>
+          <div>
+            <button
+              disabled={info.prev === null}
+              onClick={() => setPage(page - 1)}
+            >
+              Prev Page
+            </button>
+            <button
+              disabled={info.next === null}
+              onClick={() => setPage(page + 1)}
+            >
+              Next Page
+            </button>
+          </div>
+          <h1>Page: {page}</h1>
+          <pre>{JSON.stringify(info, null, 4)}</pre>
+          <pre>{JSON.stringify(characters, null, 4)}</pre>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Interfaces.ts
@@ -0,0 +1,48 @@
+export interface CharactersInfo {
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
+}
+
+export interface Characters {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+export interface Location {
+  id: number;
+  name: string;
+  type: string;
+  gender: string;
+  dimension: string;
+  residents: string[];
+  url: string;
+  created: string;
+}
+
+export interface Episode {
+  id: number;
+  name: string;
+  air_date: string;
+  episode: string;
+  characters: string[];
+  url: string;
+  created: string;
+}
